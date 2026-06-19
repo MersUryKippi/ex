febeeffecc09028b948efb35232b2cc2de4a18c8
MersUry <reginakleseva@gmail.com>
2026-06-19 05:19:19 +0300

    Update

--- core/forms.py
+++ core/forms.py
@@ -1,7 +1,5 @@
 from django import forms
-
 from .models import Item
-
 
 class ItemForm(forms.ModelForm):
     class Meta:
@@ -10,21 +8,21 @@ class ItemForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        for field in self.fields.values():
-            widget = field.widget
-            if isinstance(widget, forms.CheckboxInput):
-                widget.attrs["class"] = (widget.attrs.get("class", "") + " form-check-input").strip()
+        for name, field in self.fields.items():
+            current_widget = field.widget
+            if isinstance(current_widget, forms.CheckboxInput):
+                current_widget.attrs["class"] = f"{current_widget.attrs.get('class', '')} form-check-input".strip()
             elif isinstance(field, forms.DateTimeField):
                 field.widget = forms.DateTimeInput(
                     attrs={"type": "datetime-local", "class": "form-control"},
-                    format="%Y-%m-%dT%H:%M",
+                    format="%Y-%m-%dT%H:%M"
                 )
                 field.input_formats = ["%Y-%m-%dT%H:%M", "%Y-%m-%d %H:%M:%S"]
             elif isinstance(field, forms.DateField):
                 field.widget = forms.DateInput(
                     attrs={"type": "date", "class": "form-control"},
-                    format="%Y-%m-%d",
+                    format="%Y-%m-%d"
                 )
                 field.input_formats = ["%Y-%m-%d"]
             else:
-                widget.attrs["class"] = (widget.attrs.get("class", "") + " form-control").strip()
+                current_widget.attrs["class"] = f"{current_widget.attrs.get('class', '')} form-control".strip()
